de7879a82780a3e68a17d5eb103d93f027ab0bdb
Enes <menesilhan@gmail.com>
2022-06-07 21:44:34 +0100

    userbyname

--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 import json
 from utils.encoder import JSONEncoder
@@ -13,6 +13,20 @@ def user_route(app, users_collection):
         if user_from_db:
             # delete data we don't want to return
             del user_from_db['password']
-            return json.loads(JSONEncoder().encode({"results": user_from_db})), 200
+            return json.loads(JSONEncoder().encode({"result": user_from_db})), 200
         else:
             return jsonify({'msg': 'Profile not found'}), 404
+
+    @app.route("/api/v1/username", methods=["POST"])
+    @jwt_required()
+    def user_by_name():
+        data = request.get_json()  # store the json body request
+        print(data)
+        user_from_db = users_collection.find_one(
+            {'username': data['username']})  # find the user in the database
+        if user_from_db:
+            # delete data we don't want to return
+            del user_from_db['password']
+            return json.loads(JSONEncoder().encode({"result": user_from_db})), 200
+        else:
+            return jsonify({'msg': 'User not found'}), 404
